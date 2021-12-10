cbee7f5303187843218a69f50ef76c69d14ead0f
diegohenao819 <diegohenao819@gmail.com>
2021-12-10 18:28:26 -0500

    un commit

---
+++ crypto-dashboard/src/Components/ExchangeRate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExchangeRate() {
+    return (
+        <div className="exchange-rate">
+            Exchange Rate
+        </div>
+    )
+}
+
+export default ExchangeRate
---
+++ crypto-dashboard/src/Components/Newsfeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Newsfeed() {
+    return (
+        <div className="news-feed">
+            Newsfeed
+        </div>
+    )
+}
+
+export default Newsfeed
