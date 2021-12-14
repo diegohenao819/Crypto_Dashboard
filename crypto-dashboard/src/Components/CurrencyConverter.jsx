ebf1aa4fa13cb9d69a819dc4f47028155d899f16
diegohenao819 <diegohenao819@gmail.com>
2021-12-14 18:26:11 -0500

    .......

--- crypto-dashboard/src/Components/CurrencyConverter.jsx
+++ crypto-dashboard/src/Components/CurrencyConverter.jsx
@@ -5,20 +5,43 @@ function CurrencyConverter() {
     return (
         <div className="currency-converter">
             <h2>Currency Converter</h2>
-            <table>
-                <body>
-                    <tr>
-                        <td>Primary Currency:</td>
-                        <td>
-                            <input
-                                type="number"
-                                name="currency-amount-1"
-                                value={""}
-                            />
-                        </td>
-                    </tr>
-                </body>
-            </table>
+         <div className="input-box">
+                <table>
+                    <body>
+                        <tr>
+                            <td>Primary Currency:</td>
+                            <td>
+                                <input
+                                    type="number"
+                                    name="currency-amount-1"
+                                    value={""}
+                                />
+                            </td>
+                            <td>
+                                <select name="currency-option-1" className="currency-options" id="" value={""}>
+                                    <option value=""></option>
+                                </select>
+                            </td>
+                        </tr>
+{/* second row */}
+                        <tr>
+                            <td>Primary Currency:</td>
+                            <td>
+                                <input
+                                    type="number"
+                                    name="currency-amount-1"
+                                    value={""}
+                                />
+                            </td>
+                            <td>
+                                <select name="currency-option-1" className="currency-options" id="" value={""}>
+                                    <option value=""></option>
+                                </select>
+                            </td>
+                        </tr>
+                    </body>
+                </table>
+         </div>
             <ExchangeRate />
         </div>
     )
